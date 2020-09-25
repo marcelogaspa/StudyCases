869a866d5dfd788aacfcff17e0114aa40935ac3d
Marcelo Gasparetti <mgasparetti86@gmail.com>
2020-09-25 20:19:17 -0300

    adding frontend and backend code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,38 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 
+const users = [
+    'Marcelo',
+    'Giulia',
+    'Juliana'
+]
+
+
 app.get('/users', (request, response) => {
-    console.log('List of Users')
+    console.log('server has been started')
+
+    return response.json(users)
 })
 
-app.listen(8484)
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id)
+
+    const user = users[id]
+
+    return response.json(user)
+}) 
+
+
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'user.name',
+        lastname: 'user.lastname',
+        email: 'user.email'
+    }
+    return response.json(user)
+
+})
+
+app.listen(3333)
