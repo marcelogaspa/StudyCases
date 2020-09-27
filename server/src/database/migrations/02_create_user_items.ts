9ccfba64ecb9f92631398354e8a8f60fcbd6daa9
Marcelo Gasparetti <39143236+marcelogaspa@users.noreply.github.com>
2020-09-27 13:02:22 -0300

    migrations files

--- server/src/database/migrations/02_create_user_items.ts
+++ server/src/database/migrations/02_create_user_items.ts
@@ -2,7 +2,17 @@ import Knex from 'knex'
 
 export async function up(knex: Knex) {
     return knex.schema.createTable('user_items', table => {
+        table.increments('id').primary()
         
+        table.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('user')
+        
+        table.integer('items_id')
+            .notNullable()
+            .references('id')
+            .inTable('items')
     })
 }
 
