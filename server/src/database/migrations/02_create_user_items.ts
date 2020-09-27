e4ec3388caff1ac147b630e2089fd54c501eb085
Marcelo Gasparetti <39143236+marcelogaspa@users.noreply.github.com>
2020-09-27 12:24:12 -0300

    added items table

--- server/src/database/migrations/02_create_user_items.ts
+++ server/src/database/migrations/02_create_user_items.ts
@@ -1,9 +1,11 @@
 import Knex from 'knex'
 
-export async function up(pknex: Knex) {
-    
+export async function up(knex: Knex) {
+    return knex.schema.createTable('user_items', table => {
+        
+    })
 }
 
 export async function down(knex: Knex) {
-    
+    return knex.schema.dropTable('user_items')
 }
